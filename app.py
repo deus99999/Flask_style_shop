8dc5184ebafd0071516249704b9786d88d73b14d
deus99999 <rudenkooleksei@gmail.com>
2023-05-29 11:11:26 +0300

    refactoring function of deleting team member moved function delete_team_member to admin app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from views import home, shop, \
+from views import home, shop,\
     product_detail, show_products_of_category, about, contacts, add_items, delete_item, add_to_cart, \
     delete_from_cart, cart, clear
 # , register, confirm
